e5058c67c7c5553d3b3d1fe7f153cc397b6cc73a
KlingFelix <flxkling@gmail.com>
2024-05-07 20:26:39 +0200

    Combine Dark Photon tests in one files

--- run_test.sh
+++ run_test.sh
@@ -3,7 +3,4 @@ pytest test_Utility.py
 pytest test_Foresee.py
 
 cd DarkPhoton/
-pytest test_pion_decay.py
-pytest test_eta_decay.py
-pytest test_mix.py
-pytest test_brem.py
+pytest test_dark_photon.py
